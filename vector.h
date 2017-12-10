5dfd3546b317f0cd48f87b62a074226ccf26dfad
Ivano Bilenchi <ivanobilenchi@gmail.com>
2017-12-10 23:29:05 +0100

    Add support for equatable items

--- vector.h
+++ vector.h
@@ -10,9 +10,74 @@
 #include <stdlib.h>
 #include "array.h"
 
-#define int32_next_power_2(x) (--(x), (x)|=(x)>>1, (x)|=(x)>>2, (x)|=(x)>>4, (x)|=(x)>>8, (x)|=(x)>>16, ++(x))
+#define VECTOR_INDEX_NOT_FOUND UINT32_MAX
+
+#ifndef __vector_inline
+    #ifdef _MSC_VER
+        #define __vector_inline __inline
+    #else
+        #define __vector_inline inline
+    #endif
+#endif /* __vector_inline */
+
+#ifndef __vector_unused
+    #if (defined __clang__ && __clang_major__ >= 3) || (defined __GNUC__ && __GNUC__ >= 3)
+        #define __vector_unused __attribute__ ((__unused__))
+    #else
+        #define __vector_unused
+    #endif
+#endif /* __vector_unused */
+
+#define __vector_int32_next_power_2(x) (--(x), (x)|=(x)>>1, (x)|=(x)>>2, (x)|=(x)>>4, (x)|=(x)>>8, (x)|=(x)>>16, ++(x))
+#define __vector_identical(a, b) ((a) == (b))
+
+#define __VECTOR_IMPL_EQUATABLE(T, SCOPE, __equal_func)                                         \
+    SCOPE uint32_t vector_index_of_##T(Vector_##T vector, T item) {                             \
+        for (uint32_t i = 0, n = vector.n; i < n; ++i) {                                        \
+            if (__equal_func(vector.a[i], item)) return i;                                      \
+        }                                                                                       \
+        return VECTOR_INDEX_NOT_FOUND;                                                          \
+    }                                                                                           \
+    SCOPE uint32_t vector_index_of_identical_##T(Vector_##T vector, T item) {                   \
+        for (uint32_t i = 0, n = vector.n; i < n; ++i) {                                        \
+            if (vector.a[i] == item) return i;                                                  \
+        }                                                                                       \
+        return VECTOR_INDEX_NOT_FOUND;                                                          \
+    }                                                                                           \
+    SCOPE bool vector_push_unique_##T(Vector_##T vector, T item) {                              \
+        bool insert = vector_index_of_##T(vector, item) == VECTOR_INDEX_NOT_FOUND;              \
+        if (insert) vector_push(T, vector, item);                                               \
+        return insert;                                                                          \
+    }                                                                                           \
+    SCOPE bool vector_push_unique_identical_##T(Vector_##T vector, T item) {                    \
+        bool insert = vector_index_of_identical_##T(vector, item) == VECTOR_INDEX_NOT_FOUND;    \
+        if (insert) vector_push(T, vector, item);                                               \
+        return insert;                                                                          \
+    }                                                                                           \
+    SCOPE bool vector_remove_##T(Vector_##T vector, T item) {                                   \
+        uint32_t idx = vector_index_of_##T(vector, item);                                       \
+        if (idx != VECTOR_INDEX_NOT_FOUND) {                                                    \
+            vector_remove_at(T, vector, idx);                                                   \
+            return true;                                                                        \
+        }                                                                                       \
+        return false;                                                                           \
+    }                                                                                           \
+    SCOPE bool vector_remove_identical_##T(Vector_##T vector, T item) {                         \
+        uint32_t idx = vector_index_of_identical_##T(vector, item);                             \
+        if (idx != VECTOR_INDEX_NOT_FOUND) {                                                    \
+            vector_remove_at(T, vector, idx);                                                   \
+            return true;                                                                        \
+        }                                                                                       \
+        return false;                                                                           \
+    }
 
 #define VECTOR_DECL(T) typedef struct Vector_##T { uint32_t n, m; T *a; } Vector_##T
+
+#define VECTOR_DECL_EQUATABLE(T, __equal_func)  \
+    VECTOR_DECL(T);                             \
+    __VECTOR_IMPL_EQUATABLE(T, static __vector_inline __vector_unused, __equal_func)
+
+#define VECTOR_DECL_EQUATABLE_IDENTITY(T) VECTOR_DECL_EQUATABLE(T, __vector_identical)
 
 #define Vector(T) Vector_##T
 
@@ -41,7 +106,7 @@
 #define vector_reserve_capacity(T, vec, size) do {              \
     if ((vec).m < (size)) {                                     \
         (vec).m = (size);                                       \
-        int32_next_power_2((vec).m);                            \
+        __vector_int32_next_power_2((vec).m);                   \
         (vec).a = realloc((vec).a, sizeof(T) * (vec).m);        \
     }                                                           \
 } while (0)
@@ -50,7 +115,7 @@
 
 #define vector_shrink(T, vec) do {                          \
     (vec).m = (vec).n;                                      \
-    int32_next_power_2((vec).m);                            \
+    __vector_int32_next_power_2((vec).m);                   \
     (vec).a = realloc((vec).a, sizeof(T) * (vec).m);        \
 } while (0)
 
@@ -68,22 +133,7 @@
 
 #define vector_pop(vec) ((vec).a[--(vec).n])
 
-#define vector_push_unique(T, vec, item, equal_func) do {       \
-    bool __unique = true;                                       \
-    vector_foreach(vec, T __item, {                             \
-        if (equal_func(__item, (item))) {                       \
-            __unique = false;                                   \
-            break;                                              \
-        }                                                       \
-        if (__unique)                                           \
-            vector_push(T, (vec), (item));                      \
-    })                                                          \
-} while (0)
-
-#define __value_equals(a, b) ((a) == (b))
-#define vector_push_unique_value(T, vec, item) vector_push_unique(T, vec, item, __value_equals)
-
-#define vector_remove(T, vec, idx) do {                                 \
+#define vector_remove_at(T, vec, idx) do {                              \
     if ((idx) < (vec).n - 1) {                                          \
         size_t block_size = ((vec).n - (idx)) * sizeof(T);              \
         memmove(&((vec).a[(idx)]), &((vec).a[(idx) + 1]), block_size);  \
@@ -112,5 +162,17 @@
     }
 
 #define vector_foreach(vec, item, code) vector_iterate(vec, item, __i, code)
+
+#define vector_index_of(T, vec, item) vector_index_of_##T(vec, item)
+#define vector_index_of_identical(T, vec, item) vector_index_of_identical_##T(vec, item)
+
+#define vector_contains(T, vec, item) (vector_index_of_##T(vec, item) != VECTOR_INDEX_NOT_FOUND)
+#define vector_contains_identical(T, vec, item) (vector_index_of_identical_##T(vec, item) != VECTOR_INDEX_NOT_FOUND)
+
+#define vector_push_unique(T, vec, item) vector_push_unique_##T(vec, item)
+#define vector_push_unique_identical(T, vec, item) vector_push_unique_identical_##T(vec, item)
+
+#define vector_remove(T, vec, item) vector_remove_##T(vec, item)
+#define vector_remove_identical(T, vec, item) vector_remove_identical_##T(vec, item)
 
 #endif /* vector_h */
