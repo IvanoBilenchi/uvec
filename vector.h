336507dced30ab01ba999d49e233b24ab2c61695
Ivano Bilenchi <ivanobilenchi@gmail.com>
2018-03-02 13:09:32 +0100

    Add 'vector_ensure' and lazy accessors

--- vector.h
+++ vector.h
@@ -8,7 +8,6 @@
 
 #include <stdbool.h>
 #include <stdlib.h>
-#include "array.h"
 
 #define VECTOR_INDEX_NOT_FOUND UINT32_MAX
 
@@ -205,6 +204,7 @@
 #define vector_struct(T) struct MACRO_CONCAT(Vector_, T)
 
 #define vector_alloc(T) MACRO_CONCAT(vector_alloc_, T)()
+#define vector_ensure(T, vec) (vec ? vec : (vec = vector_alloc(T)))
 #define vector_free(T, vec) MACRO_CONCAT(vector_free_, T)(vec)
 #define vector_copy(T, vec) MACRO_CONCAT(vector_copy_, T)(vec)
 
@@ -229,20 +229,30 @@
 #define vector_count(vec) ((vec) ? (vec)->count : 0)
 
 #define vector_push(T, vec, item) MACRO_CONCAT(vector_push_, T)(vec, item)
+#define vector_push_lazy(T, vec, item) do { vector_ensure(T, vec); vector_push(T, vec, item); } while(0)
 #define vector_pop(T, vec) MACRO_CONCAT(vector_pop_, T)(vec)
 
 #define vector_remove_at(T, vec, idx) MACRO_CONCAT(vector_remove_at_, T)(vec, idx)
 #define vector_remove_all(T, vec) MACRO_CONCAT(vector_remove_all_, T)(vec)
 
-#define vector_append(T, vec, vec_to_append) MACRO_CONCAT(vector_append_array_, T)(vec, (vec_to_append)->storage, (vec_to_append)->count)
+#define vector_append(T, vec, vec_to_append) \
+    MACRO_CONCAT(vector_append_array_, T)(vec, (vec_to_append)->storage, (vec_to_append)->count)
+
+#define vector_append_lazy(T, vec, vec_to_append) \
+    do { vector_ensure(T, vec); vector_append(T, vec, vec_to_append); } while(0)
+
 #define vector_append_array(T, vec, array, n) MACRO_CONCAT(vector_append_array_, T)(vec, array, n)
 
-#define vector_iterate(T, vec, item_name, idx_name, code)                                                           \
-    if (vector_count(vec)) {                                                                                        \
-        for (uint32_t (idx_name) = 0, __n_##idx_name = (vec)->count; (idx_name) != __n_##idx_name; ++(idx_name)) {  \
-            T item_name = vector_get((vec), (idx_name));                                                            \
-            code;                                                                                                   \
-        }                                                                                                           \
+#define vector_append_array_lazy(T, vec, array, n) \
+    do { vector_ensure(T, vec); vector_append_array(T, vec, array, n); } while(0)
+
+#define vector_iterate(T, vec, item_name, idx_name, code)                               \
+    if (vector_count(vec)) {                                                            \
+        uint32_t __n_##idx_name = (vec)->count;                                         \
+        for (uint32_t (idx_name) = 0; (idx_name) != __n_##idx_name; ++(idx_name)) {     \
+            T item_name = vector_get((vec), (idx_name));                                \
+            code;                                                                       \
+        }                                                                               \
     }
 
 #define vector_foreach(T, vec, item_name, code) vector_iterate(T, vec, item_name, __i_##item_name, code)
@@ -251,13 +261,20 @@
 #define vector_index_of_identical(T, vec, item) MACRO_CONCAT(vector_index_of_identical_, T)(vec, item)
 
 #define vector_contains(T, vec, item) (MACRO_CONCAT(vector_index_of_, T)(vec, item) != VECTOR_INDEX_NOT_FOUND)
-#define vector_contains_identical(T, vec, item) (MACRO_CONCAT(vector_index_of_identical_, T)(vec, item) != VECTOR_INDEX_NOT_FOUND)
+#define vector_contains_identical(T, vec, item) \
+    (MACRO_CONCAT(vector_index_of_identical_, T)(vec, item) != VECTOR_INDEX_NOT_FOUND)
 
 #define vector_contains_all(T, vec, other_vec) MACRO_CONCAT(vector_contains_all_, T)(vec, other_vec)
-#define vector_contains_all_identical(T, vec, other_vec) MACRO_CONCAT(vector_contains_all_identical_, T)(vec, other_vec)
+#define vector_contains_all_identical(T, vec, other_vec) \
+    MACRO_CONCAT(vector_contains_all_identical_, T)(vec, other_vec)
 
 #define vector_push_unique(T, vec, item) MACRO_CONCAT(vector_push_unique_, T)(vec, item)
+#define vector_push_unique_lazy(T, vec, item) \
+    do { vector_ensure(T, vec); vector_push_unique(T, vec, item); } while(0)
+
 #define vector_push_unique_identical(T, vec, item) MACRO_CONCAT(vector_push_unique_identical_, T)(vec, item)
+#define vector_push_unique_identical_lazy(T, vec, item) \
+    do { vector_ensure(T, vec); vector_push_unique_identical(T, vec, item); } while(0)
 
 #define vector_remove(T, vec, item) MACRO_CONCAT(vector_remove_, T)(vec, item)
 #define vector_remove_identical(T, vec, item) MACRO_CONCAT(vector_remove_identical_, T)(vec, item)
@@ -270,10 +287,16 @@
         MACRO_CONCAT(vector_push_unique_, T)(vec, __item);  \
     })
 
+#define vector_append_unique_lazy(T, vec, vec_to_append) \
+    do { vector_ensure(T, vec); vector_append_unique(T, vec, vec_to_append); } while(0)
+
 #define vector_append_unique_identical(T, vec, vec_to_append)           \
     vector_foreach(T, vec_to_append, __item, {                          \
         MACRO_CONCAT(vector_push_unique_identical_, T)(vec, __item);    \
     })
+
+#define vector_append_unique_identical_lazy(T, vec, vec_to_append) \
+    do { vector_ensure(T, vec); vector_append_unique_identical(T, vec, vec_to_append); } while(0)
 
 #define vector_remove_all_from(T, vec, vec_to_remove)   \
     vector_foreach(T, vec_to_remove, __item, {          \
